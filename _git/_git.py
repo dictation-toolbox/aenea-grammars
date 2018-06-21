5918ce084e3f6d556bb1f8d71561d1cb69186c80
Dylan Chong <dylan.chong@icloud.com>
2018-06-22 10:12:01 +1200

    Finish implementing value for GitCommandRule

--- _git/_git.py
+++ _git/_git.py
@@ -18,11 +18,6 @@ from aenea import (
     Text,
 )
 
-# TODO
-import json
-
-#  from aenea import Text
-
 # TODO What is aenea.configuration.make_grammar_commands
 
 
@@ -39,6 +34,7 @@ class GitCommandRule(CompoundRule):
             command_alias = name
 
         super(GitCommandRule, self).__init__(
+            name=name,
             spec=command_alias + ' <options>',
             extras=[Repetition(
                 name='options',
@@ -52,11 +48,14 @@ class GitCommandRule(CompoundRule):
         )
 
     def value(self, node):
-        try:
-            json.dumps(node)
-            pass
-        except Exception as e:
-            print(e)
+        sequence_values = node.children[0].children[0].value()
+        option_values = sequence_values[1]
+
+        text = Text(self.name + ' ')
+        for option in option_values:
+            text += option
+
+        return text
 
 
 class GitRule(CompoundRule):
