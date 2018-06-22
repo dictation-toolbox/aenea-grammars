b5fe3c808eb23b8b8fe76dbb9787d6e51f01a887
Dylan Chong <dylan.chong@icloud.com>
2018-06-22 22:08:48 +1200

    Generate add options

--- _git/git_commands.py
+++ _git/git_commands.py
@@ -30,7 +30,11 @@ def all_commands(GitCommandRuleBuilder):
         .smart_options(['.'])
         .smart_options([
             # Generated:
-            # TODO  NEXT
+            '--', '--all,', '--chmod', '--dry-run', '--dry-run.', '--edit',
+            '--force', '--ignore-errors', '--ignore-missing',
+            '--ignore-removal', '--intent-to-add', '--interactive', '--no-all',
+            '--no-all,', '--no-ignore-removal', '--no-warn-embedded-repo',
+            '--patch', '--refresh', '--update', '--verbose',
         ])
         .build(),
 
