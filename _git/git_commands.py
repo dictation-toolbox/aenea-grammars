edffbdbd5e592889168be13b443885a04bd7ca2b
Dylan Chong <dylan.chong@icloud.com>
2018-06-22 22:06:25 +1200

    Move -- into the generated section

--- _git/git_commands.py
+++ _git/git_commands.py
@@ -7,12 +7,14 @@ Useful bash script for grabbing all of the options from a help page:
 
     git help <SOME_COMMAND> \
       | tr " " "\n" \
-      | egrep '^\--[a-zA-Z0-9].*$' \
+      | egrep '^\--.*$' \
       | egrep -v '/' \
       | egrep -v ');?$' \
       | egrep -v '\]' \
+      | egrep -v '\-{3,}' \
       | perl -pe 's/\[(.*)\]/\1/' \
       | perl -pe 's/<.*>//' \
+      | perl -pe 's/([^=]+)=.*/\1/' \
       | sort \
       | uniq \
       | perl -pe "s/(.*)(\n?)/'\1', /"
@@ -26,21 +28,24 @@ def all_commands(GitCommandRuleBuilder):
     return [
         GitCommandRuleBuilder(name='add')
         .smart_options(['.'])
+        .smart_options([
+            # Generated:
+            # TODO  NEXT
+        ])
         .build(),
 
         GitCommandRuleBuilder(name='commit', base_options=[Text(' -v')])
-        .smart_options(['.', '--'])
+        .smart_options(['.'])
         .smart_options([
             # Generated:
-            '--all', '--allow-empty', '--allow-empty-message', '--amend',
-            '--author=', '--branch', '--cleanup=', '--date=', '--dry-run',
-            '--edit', '--file=', '--fixup', '--fixup=', '--include',
-            '--interactive', '--long', '--message=', '--no-edit',
-            '--no-gpg-sign', '--no-post-rewrite', '--no-status', '--no-verify',
-            '--null', '--only', '--patch', '--porcelain', '--quiet',
-            '--reedit-message=', '--reset-author', '--reuse-message=',
-            '--short', '--signoff', '--soft', '--squash=', '--status',
-            '--template=', '--verbose',
+            '--', '--all', '--allow-empty', '--allow-empty-message', '--amend',
+            '--author', '--branch', '--cleanup', '--date', '--dry-run',
+            '--edit', '--file', '--fixup', '--include', '--interactive',
+            '--long', '--message', '--no-edit', '--no-gpg-sign',
+            '--no-post-rewrite', '--no-status', '--no-verify', '--null',
+            '--only', '--patch', '--porcelain', '--quiet', '--reedit-message',
+            '--reset-author', '--reuse-message', '--short', '--signoff',
+            '--soft', '--squash', '--status', '--template', '--verbose',
         ])
         .build(),
     ]
